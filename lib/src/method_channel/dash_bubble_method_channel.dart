540b55b0af0d262f453a5a876810a56f4d542900
Moaz El-sawaf <moazelsawaf@hotmail.com>
2023-03-10 17:41:18 +0200

    feat: :sparkles: update the plugin behavior to return false in all the methods if the platform is not Android

--- lib/src/method_channel/dash_bubble_method_channel.dart
+++ lib/src/method_channel/dash_bubble_method_channel.dart
@@ -16,14 +16,16 @@ class MethodChannelDashBubble extends DashBubblePlatform {
 
   /// The main wrapper for invoking methods on the method channel.
   ///
-  /// This method will catch any [PlatformException] or [MissingPluginException]
+  /// This method will catch any [PlatformException] or [MissingPluginException], log the error, and rethrow it.
   ///
-  /// If this method called from a platform different from Android it will do nothing.
-  Future<T?> _invokeMethod<T>(String method, [dynamic arguments]) async {
+  /// returns `false` if the platform is not Android.
+  Future<bool> _invokeMethod(String method, [dynamic arguments]) async {
     try {
-      if (!Platform.isAndroid) return null;
+      // If the platform is not Android, return false.
+      if (!Platform.isAndroid) return false;
 
-      return await methodChannel.invokeMethod<T>(method, arguments);
+      // The bang operator is used here because the method channel will always return a boolean from the native implementation.
+      return (await methodChannel.invokeMethod<bool>(method, arguments))!;
     } on PlatformException catch (_) {
       logError(
         '''PlatformException: $method
@@ -44,19 +46,19 @@ class MethodChannelDashBubble extends DashBubblePlatform {
   /// Invokes the method channel to request permission to draw over other apps.
   @override
   Future<bool> requestPermission() async {
-    return (await _invokeMethod<bool>(Constants.requestPermission))!;
+    return (await _invokeMethod(Constants.requestPermission));
   }
 
   /// Invokes the method channel to check if the permission to draw over other apps is granted.
   @override
   Future<bool> hasPermission() async {
-    return (await _invokeMethod<bool>(Constants.hasPermission))!;
+    return (await _invokeMethod(Constants.hasPermission));
   }
 
   /// Invokes the method channel to check if the bubble is currently running.
   @override
   Future<bool> isRunning() async {
-    return (await _invokeMethod<bool>(Constants.isRunning))!;
+    return (await _invokeMethod(Constants.isRunning));
   }
 
   /// Invokes the method channel to start the bubble.
@@ -73,16 +75,16 @@ class MethodChannelDashBubble extends DashBubblePlatform {
       }
     });
 
-    return (await _invokeMethod<bool>(
+    return (await _invokeMethod(
       Constants.startBubble,
-      options?.toMap() ?? BubbleOptions().toMap(),
-    ))!;
+      (options ?? BubbleOptions()).toMap(),
+    ));
   }
 
   /// Invokes the method channel to stop the bubble.
   @override
   Future<bool> stopBubble() async {
-    return (await _invokeMethod<bool>(Constants.stopBubble))!;
+    return (await _invokeMethod(Constants.stopBubble));
   }
 
   /// Logs an error message.
